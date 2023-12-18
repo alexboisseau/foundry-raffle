988dc2370f74074c0889cd17e0327d2dc5fb6640
alexboisseau <alex.boisseau79@gmail.com>
2023-12-18 11:55:26 +0100

    feat(raffle): add enterFee immutable variable and create enterRaffle function, starting to check fee

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -8,5 +8,17 @@ pragma solidity ^0.8.23;
  * @notice This contract is used to manage a raffle
  */
 contract Raffle {
+  uint256 private immutable i_enterFee;
 
+  error Raffle__NotEnoughEthSent();
+
+  constructor(uint256 _enterFee) {
+    i_enterFee = _enterFee;
+  }
+
+  function enterRaffle() external payable {
+    if (msg.value < i_enterFee) {
+      revert Raffle__NotEnoughEthSent();
+    }
+  }
 }
