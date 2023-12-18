ebf407b0335c0d99c92671a4ae4a597cf49b7035
alexboisseau <alex.boisseau79@gmail.com>
2023-12-18 11:34:43 +0100

    feat(raffle): init raffle contract

---
+++ src/Raffle.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.23;
+
+/**
+ * @title Raffle
+ * @author Alex Boisseau
+ * @notice This contract is used to manage a raffle
+ */
+contract Raffle {
+
+}
