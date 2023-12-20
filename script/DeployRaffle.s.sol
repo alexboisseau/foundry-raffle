6b52cf4fdd08cd6f04fd526e8bf728b0d71f4351
alexboisseau <alex.boisseau79@gmail.com>
2023-12-20 08:29:37 +0100

    feat(deploy raffle): create vrf subscription if subscriptionId from active network is equal to 0

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -5,29 +5,39 @@ pragma solidity ^0.8.23;
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
+import {CreateVrfSubscription} from "./Interactions.s.sol";
 
 contract DeployRaffle is Script {
   function run() external returns (Raffle, HelperConfig) {
     HelperConfig helperConfig = new HelperConfig();
-
     (
       uint256 enterFee,
       uint256 raffleIntervalInSeconds,
-      address vrfCoordinator,
+      address vrfCoordinatorV2,
       bytes32 gasLane,
       uint64 subscriptionId,
-      uint32 callbackGasLimit
+      uint32 callbackGasLimit,
+
     ) = helperConfig.activeNetworkConfig();
 
+    if (subscriptionId == 0) {
+      CreateVrfSubscription createVrfSubscription = new CreateVrfSubscription();
+      subscriptionId = createVrfSubscription.createVrfSubscription(
+        vrfCoordinatorV2
+      );
+    }
+
     vm.startBroadcast();
+
     Raffle raffle = new Raffle(
       enterFee,
       raffleIntervalInSeconds,
-      vrfCoordinator,
+      vrfCoordinatorV2,
       gasLane,
       subscriptionId,
       callbackGasLimit
     );
+
     vm.stopBroadcast();
 
     return (raffle, helperConfig);
