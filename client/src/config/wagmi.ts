72105459cd31951dfc97b94437fab655bfb9f35d
alexboisseau <alex.boisseau79@gmail.com>
2023-12-23 13:01:12 +0100

    feat(wagmi): config Alchemy as json-rpc provider in wagmi config

--- client/src/config/wagmi.ts
+++ client/src/config/wagmi.ts
@@ -1,29 +1,22 @@
 import { configureChains, createConfig } from "wagmi";
-import { foundry, goerli, mainnet } from "wagmi/chains";
-import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+import { foundry, sepolia } from "wagmi/chains";
 import { InjectedConnector } from "wagmi/connectors/injected";
-import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 
 import { publicProvider } from "wagmi/providers/public";
+import { alchemyProvider } from "wagmi/providers/alchemy";
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [sepolia, ...(import.meta.env?.MODE === "development" ? [foundry] : [])],
   [
-    mainnet,
-    ...(import.meta.env?.MODE === "development" ? [goerli, foundry] : []),
+    alchemyProvider({
+      apiKey: import.meta.env.VITE_SEPOLIA_ALCHEMY_RPC_URL,
+    }),
+    publicProvider(),
   ],
-  [publicProvider()],
 );
 
 export const config = createConfig({
-  autoConnect: true,
   connectors: [
-    new MetaMaskConnector({ chains }),
-    new CoinbaseWalletConnector({
-      chains,
-      options: {
-        appName: "wagmi",
-      },
-    }),
     new InjectedConnector({
       chains,
       options: {
