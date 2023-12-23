7a4752967302a88b3c9dd2d9f9f2b84dfefee214
alexboisseau <alex.boisseau79@gmail.com>
2023-12-23 13:01:42 +0100

    refactor(client): remove default components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,7 @@
-import { useAccount } from "wagmi";
-
-import { Account } from "./components/Account";
-import { Connect } from "./components/Connect";
-import { NetworkSwitcher } from "./components/NetworkSwitcher";
-
 export function App() {
-  const { isConnected } = useAccount();
-
   return (
     <>
-      <h1>wagmi + Vite</h1>
-
-      <Connect />
-
-      {isConnected && (
-        <>
-          <Account />
-          <hr />
-          <hr />
-          <NetworkSwitcher />
-        </>
-      )}
+      <h1>Raffle</h1>
     </>
   );
 }
