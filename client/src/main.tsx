71065e40cf20945edc9c70902db920bee689a2ae
alexboisseau <alex.boisseau79@gmail.com>
2023-12-27 12:24:18 +0100

    fix(client): update scss import in main component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import { WagmiConfig } from "wagmi";
 
 import { App } from "./App";
 import { config } from "./config/wagmi";
-import "./styles/main.css";
+import "./styles/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
