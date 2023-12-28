ab19e031647a70659e444abb0d5cf94e1e2852ba
alexboisseau <alex.boisseau79@gmail.com>
2023-12-28 16:10:56 +0100

    refactor(client): use constants in the wagmi config

--- client/src/config/wagmi.ts
+++ client/src/config/wagmi.ts
@@ -1,20 +1,17 @@
 import { configureChains } from "wagmi";
-import { foundry, sepolia } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { defaultWagmiConfig } from "@web3modal/wagmi/react";
+import { SUPPORTED_CHAINS } from "../constants/supported-chains";
 
 const walletConnectProjectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID;
 
-const { chains } = configureChains(
-  [sepolia, ...(import.meta.env?.MODE === "development" ? [foundry] : [])],
-  [
-    alchemyProvider({
-      apiKey: import.meta.env.VITE_SEPOLIA_ALCHEMY_RPC_URL,
-    }),
-    publicProvider(),
-  ],
-);
+const { chains } = configureChains(SUPPORTED_CHAINS, [
+  alchemyProvider({
+    apiKey: import.meta.env.VITE_SEPOLIA_ALCHEMY_RPC_URL,
+  }),
+  publicProvider(),
+]);
 
 const config = defaultWagmiConfig({
   chains,
