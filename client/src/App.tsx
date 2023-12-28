7eeb57f3bd058d3a969e107cb60c63adc4c1aea4
alexboisseau <alex.boisseau79@gmail.com>
2023-12-28 15:07:02 +0100

    feat(client): import connect wallet component and configure w3m theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+import "./styles/App.scss";
+
 import { WagmiConfig } from "wagmi";
 import {
   config as wagmiConfig,
@@ -5,13 +8,26 @@ import {
   walletConnectProjectId,
 } from "./config/wagmi";
 import { createWeb3Modal } from "@web3modal/wagmi/react";
+import { ConnectWallet } from "./components/ConnectWallet/ConnectWallet";
 
 createWeb3Modal({
   wagmiConfig,
   chains,
   projectId: walletConnectProjectId,
+  themeVariables: {
+    "--w3m-font-family": "RobotoMono-Regular",
+    "--w3m-accent": "#c8ff3c",
+    "--w3m-color-mix": "#c8ff3c",
+    "--w3m-color-mix-strength": 3,
+  },
 });
 
 export function App() {
-  return <WagmiConfig config={wagmiConfig}></WagmiConfig>;
+  return (
+    <div className="app">
+      <WagmiConfig config={wagmiConfig}>
+        <ConnectWallet />
+      </WagmiConfig>
+    </div>
+  );
 }
