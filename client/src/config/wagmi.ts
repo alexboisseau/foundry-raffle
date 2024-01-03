331d5b5bc0240805c1392e63f2b7045d7af7d312
alexboisseau <alex.boisseau79@gmail.com>
2024-01-03 15:58:57 +0100

    refactor(wagmi config): use create config function to use the alchemy provider

--- client/src/config/wagmi.ts
+++ client/src/config/wagmi.ts
@@ -1,21 +1,30 @@
-import { configureChains } from "wagmi";
+import { configureChains, createConfig } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
 import { alchemyProvider } from "wagmi/providers/alchemy";
-import { defaultWagmiConfig } from "@web3modal/wagmi/react";
 import { SUPPORTED_CHAINS } from "../constants/supported-chains";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+import { EIP6963Connector } from "@web3modal/wagmi";
+import { InjectedConnector } from "wagmi/connectors/injected";
 
 const walletConnectProjectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID;
 
-const { chains } = configureChains(SUPPORTED_CHAINS, [
+const { chains, publicClient } = configureChains(SUPPORTED_CHAINS, [
   alchemyProvider({
-    apiKey: import.meta.env.VITE_SEPOLIA_ALCHEMY_RPC_URL,
+    apiKey: import.meta.env.VITE_SEPOLIA_ALCHEMY_API_KEY,
   }),
   publicProvider(),
 ]);
 
-const config = defaultWagmiConfig({
-  chains,
-  projectId: walletConnectProjectId,
+const config = createConfig({
+  connectors: [
+    new WalletConnectConnector({
+      chains,
+      options: { projectId: walletConnectProjectId, showQrModal: false },
+    }),
+    new EIP6963Connector({ chains }),
+    new InjectedConnector({ chains, options: { shimDisconnect: true } }),
+  ],
+  publicClient,
 });
 
 export { config, chains, walletConnectProjectId };
