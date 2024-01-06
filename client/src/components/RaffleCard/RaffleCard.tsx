6aaf5118739a05cbbe61f9e6731d965019c0537f
alexboisseau <alex.boisseau79@gmail.com>
2024-01-06 15:20:44 +0100

    feat(enter-raffle): use enter raffle component in the raffle card component

--- client/src/components/RaffleCard/RaffleCard.tsx
+++ client/src/components/RaffleCard/RaffleCard.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-
 /** COMPONENTS */
 import { RaffleInformation } from "../RaffleInformation/RaffleInformation";
 import { ConnectWallet } from "../ConnectWallet/ConnectWallet";
 
 /** STYLES */
 import "./RaffleCard.scss";
+import { EnterRaffle } from "../EnterRaffle/EnterRaffle";
 
 export const RaffleCard = () => {
   return (
     <div className="raffle-card">
       <RaffleInformation />
       <ConnectWallet />
+      <EnterRaffle />
     </div>
   );
 };
