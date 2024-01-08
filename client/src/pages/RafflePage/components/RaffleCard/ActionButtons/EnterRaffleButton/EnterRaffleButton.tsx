a746c2554c6110211dd7d117d2b01f82f77f03d8
alexboisseau <alex.boisseau79@gmail.com>
2024-01-08 17:07:54 +0100

    feat(enter-raffle-button): remove react-hot-toast

--- client/src/pages/RafflePage/components/RaffleCard/ActionButtons/EnterRaffleButton/EnterRaffleButton.tsx
+++ client/src/pages/RafflePage/components/RaffleCard/ActionButtons/EnterRaffleButton/EnterRaffleButton.tsx
@@ -5,6 +5,7 @@ import { useEnterRaffleButton } from "./useEnterRaffleButton";
 export const EnterRaffleButton = () => {
   const { buttonIsDisabled, thereIsPendingTx, onClick } =
     useEnterRaffleButton();
+
   return (
     <>
       <Button
@@ -12,7 +13,7 @@ export const EnterRaffleButton = () => {
         value="Enter Raffle"
         onClick={onClick}
       />
-      {thereIsPendingTx && <p>Waiting for confirmation...</p>}
+      {thereIsPendingTx && <span>Tx pending ...</span>}
     </>
   );
 };
