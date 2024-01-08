32d7ae34b6706cf8e1c7493e3703d5ae47fd8225
alexboisseau <alex.boisseau79@gmail.com>
2024-01-08 15:22:24 +0100

    feat(account-button): display formatted address instead of "Account" string

--- client/src/pages/RafflePage/components/RaffleCard/ActionButtons/AccountButton/AccountButton.tsx
+++ client/src/pages/RafflePage/components/RaffleCard/ActionButtons/AccountButton/AccountButton.tsx
@@ -1,12 +1,17 @@
+import { useAccount } from "wagmi";
 import { Button } from "../../../../../../components/ui/Button/Button";
+import { formatAddress } from "../../../../../../utils/format-address";
 
 export const AccountButton = () => {
+  const { address } = useAccount();
+  const formattedAddress = formatAddress(address ?? "0x");
+
   return (
     <Button
       onClick={() => {
         console.log("lol");
       }}
-      value="Account"
+      value={formattedAddress}
     />
   );
 };
