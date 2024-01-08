ee932fda2f05d1f1e527cbec7aa185b07948b6b9
alexboisseau <alex.boisseau79@gmail.com>
2024-01-08 15:03:43 +0100

    fix(app): remove wrong import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 /** COMPONENTS */
+import { RafflePage } from "./pages/RafflePage/RafflePage";
 import { SupportedNetworks } from "./components/shared/SupportedNetworks/SupportedNetworks";
-import { RaffleCard } from "./pages/RafflePage/components/RaffleCard/RaffleCard";
 
 /** TANSTACK */
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -10,10 +10,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 /** WAGMI */
 import { WagmiProvider } from "wagmi";
 import { config } from "./config/wagmi";
-
-/** STYLES */
-import "./styles/App.scss";
-import { RafflePage } from "./pages/RafflePage/RafflePage";
 
 const queryClient = new QueryClient();
 
