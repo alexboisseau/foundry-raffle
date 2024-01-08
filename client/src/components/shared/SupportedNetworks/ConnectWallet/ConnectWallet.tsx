1b8ed0d818044a75af5106e41fce3297e562c15f
alexboisseau <alex.boisseau79@gmail.com>
2024-01-08 11:09:29 +0100

    refactor(connect-wallet): remove useless logic

--- client/src/components/shared/SupportedNetworks/ConnectWallet/ConnectWallet.tsx
+++ client/src/components/shared/SupportedNetworks/ConnectWallet/ConnectWallet.tsx
@@ -1,34 +1,14 @@
 import React, { useState } from "react";
-
-import { useAccount } from "wagmi";
-import { formatAddress } from "../../../../utils/format-address";
 import { WalletOptionsModal } from "./WalletOptionsModal/WalletOptionsModal";
 import { Button } from "../../../ui/Button/Button";
 
-const useConnectWalletButton = () => {
-  const { address, isConnected } = useAccount();
-  const [openModal, setOpenModal] = useState(false);
-
-  const buttonValue = address ? formatAddress(address) : "Connect Wallet";
-
-  return {
-    buttonValue,
-    isConnected,
-    openModal,
-    setOpenModal,
-  };
-};
-
 export const ConnectWallet = () => {
-  const { buttonValue, isConnected, openModal, setOpenModal } =
-    useConnectWalletButton();
+  const [openModal, setOpenModal] = useState(false);
 
   return (
     <>
-      <Button onClick={() => setOpenModal(true)} value={buttonValue} />
-      {openModal && !isConnected && (
-        <WalletOptionsModal onClose={() => setOpenModal(false)} />
-      )}
+      <Button onClick={() => setOpenModal(true)} value="Connect Wallet" />
+      {openModal && <WalletOptionsModal onClose={() => setOpenModal(false)} />}
     </>
   );
 };
