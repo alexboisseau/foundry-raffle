3ede26546d9265bc450a1a3328b0b769b8af51ca
alexboisseau <alex.boisseau79@gmail.com>
2024-01-09 15:44:21 +0100

    refactor(helper-config): set raffle interval to 5min for the sepolia network

--- contracts/script/HelperConfig.s.sol
+++ contracts/script/HelperConfig.s.sol
@@ -34,7 +34,7 @@ contract HelperConfig is Script {
     return
       NetworkConfig({
         enterFee: 0.01 ether,
-        raffleIntervalInSeconds: 3600,
+        raffleIntervalInSeconds: 300,
         vrfCoordinator: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
         gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
         subscriptionId: 7907,
