9a626c6ef24f07177eb0f6714e6b9d2109e69021
alexboisseau <alex.boisseau79@gmail.com>
2024-01-11 11:05:38 +0100

    refactor(switch-network): add icon to switch network btn

--- client/src/components/shared/SupportedNetworks/SwitchNetworkButton/SwitchNetworkButton.tsx
+++ client/src/components/shared/SupportedNetworks/SwitchNetworkButton/SwitchNetworkButton.tsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
 import { Button } from "../../../ui/Button/Button";
 import { NetworkOptionsModal } from "./NetworkOptionsModal/NetworkOptionsModal";
+import { TbSwitch2 } from "react-icons/tb";
 
 export const SwitchNetworkButton = () => {
   const [openModal, setOpenModal] = useState(false);
 
   return (
     <>
-      <Button value="Switch network" onClick={() => setOpenModal(true)} />
+      <Button
+        icon={<TbSwitch2 size={20} />}
+        value="Switch network"
+        onClick={() => setOpenModal(true)}
+      />
       {openModal && <NetworkOptionsModal onClose={() => setOpenModal(false)} />}
     </>
   );
