ecd3c79c0a7399b56d06a5aefd269707f5e421b4
alexboisseau <alex.boisseau79@gmail.com>
2024-01-11 11:01:31 +0100

    refactor: add icon on account button

--- client/src/pages/RafflePage/components/RaffleCard/ActionButtons/AccountButton/AccountButton.tsx
+++ client/src/pages/RafflePage/components/RaffleCard/ActionButtons/AccountButton/AccountButton.tsx
@@ -3,6 +3,7 @@ import { useAccount } from "wagmi";
 import { Button } from "../../../../../../components/ui/Button/Button";
 import { formatAddress } from "../../../../../../utils/format-address";
 import { AccountModal } from "./AccountModal/AccountModal";
+import { IoMdWallet } from "react-icons/io";
 
 export const AccountButton = () => {
   const [openModal, setOpenModal] = useState(false);
@@ -11,7 +12,11 @@ export const AccountButton = () => {
 
   return (
     <>
-      <Button onClick={() => setOpenModal(true)} value={formattedAddress} />
+      <Button
+        icon={<IoMdWallet size={20} />}
+        onClick={() => setOpenModal(true)}
+        value={formattedAddress}
+      />
       {openModal && <AccountModal onClose={() => setOpenModal(false)} />}
     </>
   );
