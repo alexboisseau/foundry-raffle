c0f531e67878c43e26c6ea4ca7e31c7931e1f28c
alexboisseau <alex.boisseau79@gmail.com>
2024-01-15 13:05:26 +0100

    refactor(account-button): improve style

--- client/src/pages/RafflePage/components/RaffleCard/ActionButtons/AccountButton/AccountButton.tsx
+++ client/src/pages/RafflePage/components/RaffleCard/ActionButtons/AccountButton/AccountButton.tsx
@@ -1,21 +1,21 @@
 import { useState } from "react";
-import { useAccount } from "wagmi";
 import { Button } from "../../../../../../components/ui/Button/Button";
-import { formatAddress } from "../../../../../../utils/format-address";
 import { AccountModal } from "./AccountModal/AccountModal";
 import { IoMdWallet } from "react-icons/io";
 
 export const AccountButton = () => {
   const [openModal, setOpenModal] = useState(false);
-  const { address } = useAccount();
-  const formattedAddress = formatAddress(address ?? "0x");
 
   return (
     <>
       <Button
         icon={<IoMdWallet size={20} />}
         onClick={() => setOpenModal(true)}
-        value={formattedAddress}
+        value="Open account modal"
+        className="account-button"
+        backgroundColor="#1e293b"
+        hoverBackgroundColor="#18212f"
+        color="#fff"
       />
       {openModal && <AccountModal onClose={() => setOpenModal(false)} />}
     </>
