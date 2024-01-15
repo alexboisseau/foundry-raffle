fe0bbecc4e6bf3982878ce45ad4628a10c360461
alexboisseau <alex.boisseau79@gmail.com>
2024-01-15 12:46:26 +0100

    refactor(button): add possibility to pass background color and color as props

--- client/src/components/ui/Button/Button.tsx
+++ client/src/components/ui/Button/Button.tsx
@@ -1,18 +1,53 @@
+import { useState } from "react";
 import "./Button.scss";
 
 export const Button = ({
+  backgroundColor,
+  hoverBackgroundColor,
+  color,
+  hoverColor,
+  className,
   disabled = false,
   icon,
   value,
   onClick,
 }: {
+  backgroundColor?: string;
+  hoverBackgroundColor?: string;
+  color?: string;
+  hoverColor?: string;
+  className?: string;
   disabled?: boolean;
   icon?: JSX.Element;
   value: string;
   onClick: () => void;
-}) => (
-  <button className="button" onClick={onClick} disabled={disabled}>
-    <span>{value}</span>
-    {icon}
-  </button>
-);
+}) => {
+  const [hover, setHover] = useState<boolean>(false);
+
+  const handleMouseEnter = () => {
+    setHover(true);
+  };
+
+  const handleMouseLeave = () => {
+    setHover(false);
+  };
+
+  return (
+    <button
+      className={className ? `button ${className}` : "button"}
+      onClick={onClick}
+      disabled={disabled}
+      style={{
+        backgroundColor: hover
+          ? hoverBackgroundColor ?? backgroundColor
+          : backgroundColor,
+        color: hover ? hoverColor ?? color : color,
+      }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <span>{value}</span>
+      {icon}
+    </button>
+  );
+};
