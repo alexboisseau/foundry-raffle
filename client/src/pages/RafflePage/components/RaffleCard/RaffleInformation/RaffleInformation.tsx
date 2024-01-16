20ac9a046cb8102df1d7f0c74eec6a4307436e24
alexboisseau <alex.boisseau79@gmail.com>
2024-01-16 10:10:40 +0100

    refactor(raffle-information): improve style of raffle information grid

--- client/src/pages/RafflePage/components/RaffleCard/RaffleInformation/RaffleInformation.tsx
+++ client/src/pages/RafflePage/components/RaffleCard/RaffleInformation/RaffleInformation.tsx
@@ -14,24 +14,39 @@ import { formatEther } from "viem";
 /** UTILS */
 import { formatAddress } from "../../../../../utils/format-address";
 import { useGetRafflePlayers } from "../../../../../hooks/useGetRafflePlayers";
+import { useBreakpoints } from "../../../../../hooks/useBreakpoints";
 
 export const RaffleInformation = () => {
   const enterFee = useGetRaffleEnterFee();
   const lastWinner = useGetRaffleLastWinner();
   const { players, userTickets } = useGetRafflePlayers();
+  const { isSm } = useBreakpoints();
 
   const formattedEnterFee = formatEther(enterFee);
-  const formattedAddress = formatAddress(lastWinner);
+  const formattedAddress = isSm ? lastWinner : formatAddress(lastWinner);
 
   return (
     <div className="raffle-information">
       <RaffleInformationCard
+        gridAreaClassName="enter-fee"
         label="Enter Fee (ETH)"
         value={formattedEnterFee}
       />
-      <RaffleInformationCard label="Players" value={players.length} />
-      <RaffleInformationCard label="Last Winner" value={formattedAddress} />
-      <RaffleInformationCard label="Your tickets" value={userTickets} />
+      <RaffleInformationCard
+        gridAreaClassName="players"
+        label="Players"
+        value={players.length}
+      />
+      <RaffleInformationCard
+        gridAreaClassName="last-winner"
+        label="Last Winner"
+        value={formattedAddress}
+      />
+      <RaffleInformationCard
+        gridAreaClassName="tickets"
+        label="Your tickets"
+        value={userTickets}
+      />
     </div>
   );
 };
--- client/src/pages/RafflePage/components/RaffleCard/RaffleInformation/RaffleInformationCard.tsx
+++ client/src/pages/RafflePage/components/RaffleCard/RaffleInformation/RaffleInformationCard.tsx
@@ -1,12 +1,14 @@
 export const RaffleInformationCard = ({
+  gridAreaClassName,
   label,
   value,
 }: {
+  gridAreaClassName: string;
   label: string;
   value: any;
 }) => {
   return (
-    <div className="raffle-information-card">
+    <div className={`raffle-information-card ${gridAreaClassName}`}>
       <p className="label">{label}</p>
       <p className="value">{value}</p>
     </div>
