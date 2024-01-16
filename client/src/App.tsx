4d8277391e0ac8eb455b82f7c74c64281ddaab52
alexboisseau <alex.boisseau79@gmail.com>
2024-01-16 10:49:37 +0100

    refactor(app): clean imports and use inline function

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,22 @@
-import React from "react";
-
-/** COMPONENTS */
 import { RafflePage } from "./pages/RafflePage/RafflePage";
-
-/** TANSTACK */
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-/** WAGMI */
 import { WagmiProvider } from "wagmi";
 import { config } from "./config/wagmi";
-
 import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient();
 
-export function App() {
-  return (
-    <div className="app">
-      <WagmiProvider config={config}>
-        <QueryClientProvider client={queryClient}>
-          <RafflePage />
-        </QueryClientProvider>
-      </WagmiProvider>
-      <Toaster
-        toastOptions={{
-          className: "toast",
-        }}
-      />
-    </div>
-  );
-}
+export const App = () => (
+  <div className="app">
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RafflePage />
+      </QueryClientProvider>
+    </WagmiProvider>
+    <Toaster
+      toastOptions={{
+        className: "toast",
+      }}
+    />
+  </div>
+);
