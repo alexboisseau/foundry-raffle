806880ae5400f4d45a3dcd71b2291ae10f02d98a
alexboisseau <alex.boisseau79@gmail.com>
2024-01-16 10:43:07 +0100

    refactor(raffle-card): clean imports and use inline function

--- client/src/pages/RafflePage/components/RaffleCard/RaffleCard.tsx
+++ client/src/pages/RafflePage/components/RaffleCard/RaffleCard.tsx
@@ -1,15 +1,10 @@
-/** COMPONENTS */
 import { RaffleInformation } from "./RaffleInformation/RaffleInformation";
 import { ActionButtons } from "./ActionButtons/ActionButtons";
-
-/** STYLES */
 import "./RaffleCard.scss";
 
-export const RaffleCard = () => {
-  return (
-    <div className="raffle-card">
-      <RaffleInformation />
-      <ActionButtons />
-    </div>
-  );
-};
+export const RaffleCard = () => (
+  <div className="raffle-card">
+    <RaffleInformation />
+    <ActionButtons />
+  </div>
+);
