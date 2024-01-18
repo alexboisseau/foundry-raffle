c01d33ce468181683569ce29eb56504fc2dcf355
alexboisseau <alex.boisseau79@gmail.com>
2024-01-18 15:50:28 +0100

    chore(radix): install radix-ui lib

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 
 import { App } from "./App";
+import "@radix-ui/themes/styles.css";
 import "./styles/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
