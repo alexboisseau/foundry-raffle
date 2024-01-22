cb963c302836f6b16e8b3281a4d39b80d3d71c94
alexboisseau <alex.boisseau79@gmail.com>
2024-01-22 15:24:16 +0100

    feat(theme-provider): create a theme provider component which wraps the app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,6 +9,7 @@ import { App } from "./App";
 
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
+import { ThemeProvider } from "./components/shared/ThemeProvider/ThemeProvider";
 
 import "./styles/main.scss";
 
@@ -16,12 +17,14 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Theme>
-      <WagmiProvider config={config}>
-        <QueryClientProvider client={queryClient}>
-          <App />
-        </QueryClientProvider>
-      </WagmiProvider>
-    </Theme>
+    <ThemeProvider attribute="class">
+      <Theme>
+        <WagmiProvider config={config}>
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
+        </WagmiProvider>
+      </Theme>
+    </ThemeProvider>
   </React.StrictMode>,
 );
