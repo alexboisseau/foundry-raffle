072a76999ccfb2577c0018b47cf6876698ba5ce6
alexboisseau <alex.boisseau79@gmail.com>
2024-01-24 17:45:40 +0100

    refactor(next-withdraw): move the interval logic into the hook instead of the component

--- client/src/pages/RafflePage/components/NextRaffleWithdraw/NextRaffleWithdraw.tsx
+++ client/src/pages/RafflePage/components/NextRaffleWithdraw/NextRaffleWithdraw.tsx
@@ -1,40 +1,13 @@
-import { useEffect, useState } from "react";
 import { useNextRaffleWithdraw } from "./useNextRaffleWithdraw";
-import { formatMilliseconds } from "../../../../utils/format-milliseconds";
 import { Box, Text } from "@radix-ui/themes";
 import "./NextRaffleWithdraw.scss";
 
 export const NextRaffleWithdraw = () => {
-  const { nextWithdraw } = useNextRaffleWithdraw();
-  const [nextRaffleWithdraw, setNextRaffleWithdraw] = useState("");
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const currentTimeStamp = Date.now();
-      const timeUntilNextWithdraw = nextWithdraw - currentTimeStamp;
-
-      try {
-        const formattedTime = formatMilliseconds(timeUntilNextWithdraw);
-        setNextRaffleWithdraw(`Next withdraw in ${formattedTime}`);
-      } catch (error) {
-        if (timeUntilNextWithdraw <= 0) {
-          setNextRaffleWithdraw(
-            "Calculation is in progress, a winner should be announced soon!",
-          );
-
-          return;
-        } else {
-          setNextRaffleWithdraw(`Next withdraw in 00:00:00`);
-        }
-      }
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, [nextWithdraw]);
+  const { nextWithdrawText } = useNextRaffleWithdraw();
 
   return (
     <Box className="next-raffle-withdraw">
-      <Text>{nextRaffleWithdraw}</Text>
+      <Text>{nextWithdrawText}</Text>
     </Box>
   );
 };
--- client/src/pages/RafflePage/components/NextRaffleWithdraw/useNextRaffleWithdraw.ts
+++ client/src/pages/RafflePage/components/NextRaffleWithdraw/useNextRaffleWithdraw.ts
@@ -1,12 +1,40 @@
+import { useEffect, useState } from "react";
 import { useGetRaffleIntervalInSeconds } from "../../../../hooks/useGetRaffleIntervalInSeconds";
 import { useGetRaffleLastWithdrawInSeconds } from "../../../../hooks/useGetRaffleLastWithdrawInSeconds";
+import { formatMilliseconds } from "../../../../utils/format-milliseconds";
 
 export const useNextRaffleWithdraw = () => {
   const raffleIntervalInSeconds = useGetRaffleIntervalInSeconds();
   const lastWithdrawInSeconds = useGetRaffleLastWithdrawInSeconds();
-  const nextWithdraw = (lastWithdrawInSeconds + raffleIntervalInSeconds) * 1000;
+  const [nextWithdrawText, setNextWithdrawText] = useState("");
+  const nextWithdrawInMs =
+    (lastWithdrawInSeconds + raffleIntervalInSeconds) * 1000;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const currentTimeStamp = Date.now();
+      const timeUntilNextWithdraw = nextWithdrawInMs - currentTimeStamp;
+
+      try {
+        const formattedTime = formatMilliseconds(timeUntilNextWithdraw);
+        setNextWithdrawText(`Next withdraw in ${formattedTime}`);
+      } catch (error) {
+        if (timeUntilNextWithdraw <= 0) {
+          setNextWithdrawText(
+            "Calculation is in progress, a winner should be announced soon!",
+          );
+
+          return;
+        } else {
+          setNextWithdrawText(`Next withdraw in 00:00:00`);
+        }
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [nextWithdrawInMs]);
 
   return {
-    nextWithdraw,
+    nextWithdrawText,
   };
 };
